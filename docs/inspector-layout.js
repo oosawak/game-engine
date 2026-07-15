d8c6717ac077a3a89d5d294ca36115ba1dac1a2c
K.Oosawa <oosawakou@gmail.com>
2026-07-15 01:25:51 +0000

    make inspector layout config-driven

---
+++ docs/inspector-layout.js
@@ -0,0 +1,11 @@
+export const INSPECTOR_LAYOUT = {
+  default: ["transform", "script"],
+  Cube: ["transform", "script"],
+  Empty: ["transform", "script"],
+  WASM: ["transform", "script"],
+  PNG: ["transform", "script", "png"],
+  VRM: ["transform", "script", "vrm"],
+  Camera: ["transform", "script", "camera"],
+  Light: ["transform", "script", "light"],
+  ADVMNG: ["transform", "script", "advmng"],
+};
